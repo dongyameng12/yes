f4143306bd31039fd6528d9e91c1757c693e45bf
yangtao <1509440167@qq.com>
2019-05-28 18:46:14 +0800

    吧吧v

---
+++ js/main.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function(){	
+	var target = document.getElementById('province1');
+	if (target.addEventListener) {
+		target.addEventListener('click', selectedHandler, false);
+	} else if (target.attachEvent) {
+		target.attachEvent('click', selectedHandler);
+	}
+	maphover();
+});
+
+function selectedHandler(){	
+	if($("#province1")[0].value!='')
+	{
+		var selectedText = $("#province1")[0].value;
+		$(".city").each(function(){
+			$(this).children("div").hide();
+			$(this).removeClass("hover");
+		})
+		$(".city").each(function(){
+			if($(this)[0].textContent==selectedText){
+				$(this).addClass("hover").children("div").show();
+			}
+		})
+	}			
+};
+
+function maphover(){	
+	var self = "";
+	$(".city").hover(
+		function(){
+			self = $(this);
+			self.addClass("hover").children("div").show();
+		},
+		function(){
+			self = $(this);
+			self.children("div").hide();
+			self.removeClass("hover");
+		}
+	);
+	$(".city").click(
+		function(){
+			self = $(this);
+			self.addClass("hover").children("div").show();
+		},
+		function(){
+			self = $(this);
+			self.children("div").hide();
+			self.removeClass("hover");
+		}
+	);		
+};
